611da27d88fb9f18c72665815b4b8b5d52016891
DanyVeneno <danieledjv@gmail.com>
2024-04-30 01:52:28 -0600

    solucionando runtime3.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
 export default defineConfig({
   ViewTransitions: true,
   output: "server",
-  adapter: vercel(),
-  integrations: [tailwind(), react()]
+  adapter: netlify(),
+  integrations: [tailwind(), react()],
 });
